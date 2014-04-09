6be6eb02000b1c47dfb9ef9944c4bef8594348da
rr.kaliyaperumal@gmail.com <whoisnext5>
2014-04-09 16:55:03 +0200

    Memory  error fixed

--- script/converters/converter.rb
+++ script/converters/converter.rb
@@ -1,11 +1,8 @@
 require 'rdf'
-require 'rdf/turtle'
 require 'rdf/allegro_graph'
 require 'slop'
 require 'logger'
-require 'rdf/trig'
-require 'linkeddata'
-require 'rdf/nquads'
+require 'zlib'
 
 class RDF_Converter
 
@@ -14,30 +11,27 @@ class RDF_Converter
   def initialize (rdfNs, npNs, prefix)
 
     @options = get_options
-    #@base = RDF::Vocabulary.new("#{@options[:base_url]}/HD_associations/")
-    @base = RDF::Vocabulary.new(@options[:base_url])
-    @graphHashKey = 0
-    @graphHash = Hash.new
-    @saveFiles = false
+    $base = RDF::Vocabulary.new(@options[:base_url])
+
+    $saveFiles = false
 
     # tracking converter progress
     @line_number = 0 # incremented after a line is read from input
     @row_index = 0 # incremented before a line is converted.
 
 
-    @logger = Logger.new(STDOUT)
-    @logger.level = Logger::INFO
+    $logger = Logger.new(STDOUT)
+    $logger.level = Logger::INFO
 
-    @RDF = rdfNs
-    @NP = npNs
-    @prefixes = prefix
+    $RDF = rdfNs
+    $NP = npNs
+    $prefixes = prefix
   end
 
   def convert
     File.open(@options[:input], 'r') do |f|
 
       time_start = Time.now.utc
-
 
       while line = f.gets
         @line_number += 1
@@ -48,17 +42,15 @@ class RDF_Converter
         end
 
         if @line_number % 10 == 0
-          #@logger.info("============ running time: #{(Time.now.utc - time_start).to_s} ============")
-          #puts ("#{(Time.now.utc - time_start).to_s}")
+          #$logger.info("============ running time: #{(Time.now.utc - time_start).to_s} ============")
         end
 
       end
-
-      @logger.info("============ running time total: #{(Time.now.utc - time_start).to_s} ============")
+      $logger.info("============ running time total: #{(Time.now.utc - time_start).to_s} ============")
     end
 
-    if @saveFiles
-      writeFile()
+    if $saveFiles
+      closeFile()
     end
 
   end
@@ -93,12 +85,11 @@ class RDF_Converter
 
   private
   def create_main_graph(nanopub, assertion, provenance, publication_info)
-    # old code replace @base with nanopub
     save(nanopub, [
-        [nanopub, @RDF.type, @NP.Nanopublication],
-        [nanopub, @NP.hasAssertion, assertion],
-        [nanopub, @NP.hasProvenance, provenance],
-        [nanopub, @NP.hasPublicationInfo, publication_info]
+        [nanopub, $RDF.type, $NP.Nanopublication],
+        [nanopub, $NP.hasAssertion, assertion],
+        [nanopub, $NP.hasProvenance, provenance],
+        [nanopub, $NP.hasPublicationInfo, publication_info]
     ])
   end
 
@@ -108,83 +99,57 @@ class RDF_File_Converter < RDF_Converter
 
   def initialize(rdfNs, npNs, prefix)
     super(rdfNs, npNs, prefix)
-    @saveFiles = true
-    @repo = RDF::Repository.new
-    @totalStatements = 0
-    @fileCreated = 0
-    @NoOfStatements = 0
-    @file = nil
+    $saveFiles = true
+    $totalStatements = 0
+    $filesCreated = 0
+    $NoOfStatements = 0
+    $file = nil
+    $time_start = 0
+
   end
 
 
 
-  def save(ctx, triples)
-    triples.each do |s, p, o|
-      #@repo  << RDF::Statement.new(s.to_uri, p, o, :context => ctx.to_uri)
+  def save(context, triples)
+    triples.each do |subject, predicate, object|
 
-      if @NoOfStatements == 0
+      if $NoOfStatements == 0
 
-        @fileCreated += 1
-        outputFile =  "#{@options[:output]}_#{@fileCreated}.nq.gz"
-        @file = Zlib::GzipWriter.open(outputFile);
-
+        $filesCreated += 1
+        outputFile =  "#{@options[:output]}_#{$filesCreated}.nq.gz"
+        $file = Zlib::GzipWriter.open(outputFile)
+        $time_start = Time.now.utc
       end
 
-      #line = "<#{s.to_uri}><#{p.to_uri}><#{o.to_uri}><#{ctx.to_uri}>"
-
-      @file << RDF::Statement.new(s.to_uri, p, o, :context => ctx.to_uri)
-      #puts "This is object ==   #{o.}"
-      #@file << ("<#{s.to_uri}> <#{p}> #{o} <#{ctx.to_uri}> .")
-      @file << "\n"
-
-      @NoOfStatements += 1
-
-      if @NoOfStatements > 1000000
-        writeFile()
-
+      #@file << RDF::Statement(subject.to_uri, predicate, object, :context => context.to_uri)
+      if object.literal?
+        objectLiteral = ("\"#{object.to_s}\"^^<#{object.datatype}>")
+        $file << ("<#{subject.to_uri}> <#{predicate.to_uri}> #{objectLiteral} <#{context.to_uri}> .")
+      else
+        $file << ("<#{subject.to_uri}> <#{predicate.to_uri}> <#{object.to_s}> <#{context.to_uri}> .")
       end
 
+      $file << "\n"
 
-      #if @repo.size > 1000000
-        #writeFile();
-      #end
+      $NoOfStatements += 1
+
+      if $NoOfStatements > 1000000
+        closeFile()
+      end
     end
 
   end
 
-  def writeFile()
+  def closeFile()
 
-    @totalStatements = @totalStatements + @NoOfStatements
-    @NoOfStatements = 0
+    $totalStatements = $totalStatements + $NoOfStatements
+    $NoOfStatements = 0
 
-    puts "Total no of statements in a file #{@totalStatements}"
-    if @file != nil
-      @file.close
+    puts "No of statements in a file #{$totalStatements}"
+    if $file != nil
+      $file.close
     end
-
-
-    #@fileCreated += 1
-
-    #if @repo.size > 0
-
-     # @totalStatements = @totalStatements + @repo.size
-
-      #outputFile =  "#{@options[:output]}_#{@fileCreated}.nq.gz"
-      #file = Zlib::GzipWriter.open(outputFile); # File.open(@options[:output], "w")
-      #file << @repo.dump(:nquads)
-      #file.close
-      #File.open(outputFile, "w") {|f| f << @repo.dump(:nquads)}
-      #@repo.clear
-      #puts "No 0f Statements = #{@totalStatements}"
-    #end
-  end
-
-  def writeSingleFile()
-    @fileCreated +=1
-
-    outputFile =  "#{@options[:output]}.nq"
-    File.open(outputFile, "w") {|f| f << @repo.dump(:nquads)}
-
+    $logger.info((Time.now.utc - $time_start).to_s)
   end
 
 end
@@ -211,10 +176,9 @@ class RDF_Nanopub_Converter < RDF_Converter
   end
 
   protected
-  def save(ctx, triples)
-    ctx_uri = ctx.to_uri
-    triples.each do |s, p, o|
-      @repository.insert([s.to_uri, p, o, ctx_uri])
+  def save(context, triples)
+    triples.each do |subject, predicate, object|
+      @repository.insert([subject.to_uri, predicate, object, context.to_uri])
     end
   end
 
--- script/converters/fantom5.rb
+++ script/converters/fantom5.rb
@@ -1,10 +1,14 @@
+# Example arguments (Shell script):
+# ruby fantom5.rb --input /home/raja/fantom5/fantom5DS.txt --output /home/raja/fantom5/fantom5-np/fantom5 --subtype ff_expressions --username raja --password raja --catalog system --repository test --host localhost --port 10035
+#
+# Example arguments (Rubymine):
+# '-help --input /home/raja/fantom5/fantom5DS.txt --output /home/raja/fantom5/fantom5-np/fantom5 --subtype ff_expressions --username raja --password raja --catalog system --repository test --host localhost --port 10035'
+
 require 'rdf'
 require 'slop'
 require_relative 'converter'
-#require_relative 'converterSplitRun'
 
 class Fantom5_Nanopub_Converter < RDF_File_Converter
-
 
   # Define some useful RDF vocabularies.(Note: Define subclass RDF vocabularies here)
   FOAF = RDF::FOAF
@@ -19,12 +23,11 @@ class Fantom5_Nanopub_Converter < RDF_File_Converter
   PAV = RDF::Vocabulary.new('http://swan.mindinformatics.org/ontologies/1.2/pav/')
   NP = RDF::Vocabulary.new('http://www.nanopub.org/nschema#')
   SIO = RDF::Vocabulary.new('http://semanticscience.org/resource/')
-  HDA = RDF::Vocabulary.new('http://rdf.biosemantics.org/dataset/huntingtons_disease_associations#')
-  VO = RDF::Vocabulary.new('http://hgvs.org/ontology/Variation#')
-  LOVD = RDF::Vocabulary.new('http://lovd.lovd.nl/')
   FF = RDF::Vocabulary.new('http://purl.obolibrary.org/obo/FF_')
   IAO = RDF::Vocabulary.new('http://purl.obolibrary.org/obo/')
   FFU = RDF::Vocabulary.new('http://rdf.biosemantics.org/ontologies/fantom5Units#')
+  FANTOM5 = RDF::Vocabulary.new('http://rdf.biosemantics.org/data/riken/fantom5/data#')
+  $base = RDF::Vocabulary.new('http://rdf.biosemantics.org/nanopubs/riken/fantom5/')
 
 
   def initialize
@@ -42,63 +45,39 @@ class Fantom5_Nanopub_Converter < RDF_File_Converter
         :iao => IAO,
         :so => SO,
         :rso => RSO,
-        nil => @base
+        nil => $base
     }
 
-    @ffont = File.read('ffont.rb').split(", ")
+    # read all cell types
+    $ffont = File.read('ffont.rb').split(", ")
 
-    @npVersion = 1.0
-    #@nm_id = "NM_000070.2"
-    #@gene_id = 825
-    #@chromosome = "chr15"
+    $npVersion = 1.0
 
     super(RDF, NP, prefixes)
 
   end
 
-
-
-
   @@AnnotationSignChars = '+-'
-
-  FANTOM5 = RDF::Vocabulary.new('http://rdf.biosemantics.org/data/riken/fantom5/data#')
-
-
 
   def convert_header_row(row)
     # do nothing
+    # ignore summary rows
   end
 
   def convert_row(row)
-    # ignore summary rows
-    #begin
-=begin
-    if (
-    #(row =~ /^chr11:/) || (row =~ /^chr15:/) || (row =~ /^chr3:/) || (row =~ /^chr2:/)
-        #||
-        #(row =~ /^chr1:/) || (row =~ /^chr17:/) || (row =~ /^chr4:/) || (row =~ /^chr5:/)
-        #||
-        (row =~ /^chr13:/)
-    )
-=end
 
-    #if row =~ /^chr1:/
-      @row_index += 1
-      annotation, shortDesc, description, transcriptAssociation, geneEntrez, geneHgnc, geneUniprot, *samples = row.split("\t")
-      #puts "#{annotation} ## #{shortDesc} ## #{description} ## #{transcriptAssociation} ## #{geneEntrez} ## #{geneHgnc} ## #{geneUniprot}"
-      case @options[:subtype]
-        when 'cage_clusters'
-          create_class1_nanopub(annotation)
-        when 'gene_associations'
-          create_class2_nanopub(annotation, transcriptAssociation, geneEntrez, geneHgnc, geneUniprot)
-        when 'ff_expressions'
-          create_class3_nanopub(annotation, samples)
-      end
-    #end
-    #rescue
-     # puts "Some error"
+    @row_index += 1
+    annotation, shortDesc, description, transcriptAssociation, geneEntrez, geneHgnc, geneUniprot, *samples = row.split("\t")
 
-    #end
+    case @options[:subtype]
+      when 'cage_clusters'
+        create_class1_nanopub(annotation)
+      when 'gene_associations'
+        create_class2_nanopub(annotation, transcriptAssociation, geneEntrez, geneHgnc, geneUniprot)
+      when 'ff_expressions'
+        create_class3_nanopub(annotation, samples)
+    end
+
   end
 
   protected
@@ -119,7 +98,7 @@ class Fantom5_Nanopub_Converter < RDF_File_Converter
       chromosome, start_pos, end_pos, sign = $1, $2, $3, $4
 
       # setup nanopub
-      nanopub = RDF::Vocabulary.new(@base['cage_clusters/' + @row_index.to_s])
+      nanopub = $base['cage_clusters/' + @row_index.to_s]
       assertion = nanopub['#assertion']
       provenance = nanopub['#provenance']
       publicationInfo = nanopub['#publicationInfo']
@@ -146,7 +125,7 @@ class Fantom5_Nanopub_Converter < RDF_File_Converter
       # publication info graph
       create_publication_info_graph(publicationInfo, nanopub)
 
-      puts "inserted nanopub <#{nanopub}>"
+      #puts "inserted nanopub <#{nanopub}>"
     else
       puts "Unknown annotation format: #{annotation}"
     end
@@ -159,19 +138,12 @@ class Fantom5_Nanopub_Converter < RDF_File_Converter
       transcripts = transcripts.split(',')
 
       # setup nanopub
-      nanopub = RDF::Vocabulary.new(@base['gene_associations/' + @row_index.to_s])
+      nanopub = RDF::Vocabulary.new($base['gene_associations/' + @row_index.to_s])
       assertion = nanopub['#assertion']
       provenance = nanopub['#provenance']
       publicationInfo = nanopub['#publicationInfo']
 
-      # main graph
-      create_main_graph(nanopub, assertion, provenance, publicationInfo)
-
-      # assertion graph
-
       transcriptForTss = transcripts[0]
-
-
 
       for transcript in transcripts
         if transcript =~ /^NM_/  #Note: NM transcript is preferred for tss_region url.
@@ -182,6 +154,10 @@ class Fantom5_Nanopub_Converter < RDF_File_Converter
       tss = FANTOM5["tss_#{transcriptForTss}"]
       entrez_id = geneEntrez.split(':')[1]
 
+      # main graph
+      create_main_graph(nanopub, assertion, provenance, publicationInfo)
+
+      # assertion graph
       save(assertion, [
           [FANTOM5[annotation], RSO.is_observation_of, tss],
           [tss, RDF.type, SO.TSS_region],
@@ -194,7 +170,7 @@ class Fantom5_Nanopub_Converter < RDF_File_Converter
       # publication info graph
       create_publication_info_graph(publicationInfo, nanopub)
 
-      puts "inserted nanopub <#{nanopub}>"
+      #puts "inserted nanopub <#{nanopub}>"
     else
       if transcriptAssociation != 'NA'
         puts "Unknown transcript association format: #{transcriptAssociation}"
@@ -206,41 +182,33 @@ class Fantom5_Nanopub_Converter < RDF_File_Converter
 
   protected
   def create_class3_nanopub(annotation, samples)
-        # read all cell types
-
-
     if samples.is_a?(Array)
+
       samples.each_with_index { |tpm, sample_index|
 
-        if @ffont[sample_index] == @options[:celltype]
+        if tpm.to_f > 0 #$ffont[sample_index] == @options[:celltype]
 
-          #puts samples
-          # setup nanopub
-          nanopub = RDF::Vocabulary.new(@base['ff_expressions/' + @row_index.to_s + '/' + sample_index.to_s])
-          assertion = nanopub['#assertion']
-          provenance = nanopub['#provenance']
-          publicationInfo = nanopub['#publicationInfo']
+          nanopub = RDF::URI.new("http://rdf.biosemantics.org/nanopubs/riken/fantom5/ff_expressions/#{@row_index.to_s}/#{sample_index.to_s}")
+          assertion = RDF::URI.new("http://rdf.biosemantics.org/nanopubs/riken/fantom5/ff_expressions/#{@row_index.to_s}/#{sample_index.to_s}#assertion")
+          provenance = RDF::URI.new("http://rdf.biosemantics.org/nanopubs/riken/fantom5/ff_expressions/#{@row_index.to_s}/#{sample_index.to_s}#provenance")
+          publicationInfo = RDF::URI.new("http://rdf.biosemantics.org/nanopubs/riken/fantom5/ff_expressions/#{@row_index.to_s}/#{sample_index.to_s}#provenance")
 
-          measurementValue = FANTOM5['measurementValue_' +@row_index.to_s+ '_' + sample_index.to_s]
-
-
+          measurementValue = RDF::URI.new("http://rdf.biosemantics.org/data/riken/fantom5/data#measurementValue/#{@row_index.to_s}/#{sample_index.to_s}")
+          annotationURI = RDF::URI.new('http://rdf.biosemantics.org/data/riken/fantom5/data#'+annotation)
 
           # main graph
           create_main_graph(nanopub, assertion, provenance, publicationInfo)
 
           # assertion graph
-
           save(assertion, [
-              #[FANTOM5[annotation], RSO.observed_in, FF[ffont[sample_index]]],
-              #[FANTOM5[annotation], FF.hasTPM, tpm],
-              [FANTOM5[annotation],SO['associated_with'],measurementValue],
+              [annotationURI, SO['associated_with'], measurementValue],
               # IAO_0000032 = scalar measurement datum
               [measurementValue, RDF.type, IAO.IAO_0000032],
               # IAO_0000004 = has_measurement_value
               [measurementValue, IAO['IAO_0000004'], RDF::Literal.new(tpm.to_f, :datatype => RDF::XSD.double)],
               # IAO_0000039 = has_measurement_unit_label
               [measurementValue, IAO['IAO_0000039'], FFU.TPM],
-              [FANTOM5[annotation], RSO.observed_in, FF[@ffont[sample_index]]]
+              [annotationURI, RSO.observed_in, FF[$ffont[sample_index]]]
           ])
 
           # provenance graph
@@ -248,10 +216,6 @@ class Fantom5_Nanopub_Converter < RDF_File_Converter
 
           # publication info graph
           create_publication_info_graph(publicationInfo, nanopub)
-
-          #puts "#{transcriptAssociation}, #{geneEntrez}, #{geneHgnc}, #{geneUniprot}"
-          #puts "inserted nanopub <#{nanopub}>"
-        #}
 
         end
       }
@@ -273,10 +237,10 @@ class Fantom5_Nanopub_Converter < RDF_File_Converter
   def create_publication_info_graph(publicationInfo, nanopub)
     save(publicationInfo, [
         [nanopub, PAV.authoredBy, RDF::URI.new('http://rdf.biosemantics.org/data/riken/fantom5/project')],
-        [nanopub, PAV.createdBy, 'Andrew Gibson'],
-        [nanopub, PAV.createdBy, 'Mark Thompson'],
-        [nanopub, PAV.createdBy, 'Zuotian Tatum'],
-        [nanopub, PAV.createdBy, 'Rajaram Kaliyaperumal'],
+        [nanopub, PAV.createdBy, RDF::Literal.new('Andrew Gibson', :datatype => XSD.string)] ,
+        [nanopub, PAV.createdBy, RDF::Literal.new('Mark Thompson', :datatype => XSD.string)],
+        [nanopub, PAV.createdBy, RDF::Literal.new('Zuotian Tatum', :datatype => XSD.string)],
+        [nanopub, PAV.createdBy, RDF::Literal.new('Rajaram Kaliyaperumal', :datatype => XSD.string)],
         [nanopub, DC.rights, RDF::URI.new('http://creativecommons.org/licenses/by/3.0/')],
         [nanopub, DC.rightsHolder, RDF::URI.new('http://www.riken.jp/')],
         [nanopub, DC.created, RDF::Literal.new(Time.now.utc, :datatype => XSD.dateTime)]
